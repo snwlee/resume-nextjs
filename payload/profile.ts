f549273c5a5267e6c2230f7f8a82e95e13488a9e
snwlee <pgrrr119@gmail.com>
2025-08-21 00:17:09 +0900

    export

--- payload/profile.ts
+++ payload/profile.ts
@@ -1,18 +1,15 @@
-import { faEnvelope, faPhone, faRss } from '@fortawesome/free-solid-svg-icons';
-import { faFacebook, faGithub } from '@fortawesome/free-brands-svg-icons';
-
+import { faEnvelope, faRss } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faBell } from '@fortawesome/free-regular-svg-icons';
+
 import { IProfile } from '../component/profile/IProfile';
-import image from '../asset/sample_tux.png';
 
 const profile: IProfile.Payload = {
   disable: false,
 
   image: 'https://avatars.githubusercontent.com/u/82517133?v=4',
-  // image, // Remove this line
   name: {
     title: '이선우',
-    // small: '(선우)', // Remove this line
   },
   contact: [
     {
@@ -31,11 +28,11 @@ const profile: IProfile.Payload = {
       icon: faGithub,
     },
   ],
-  // notice: { // Remove this entire notice block
-  //   title:
-  //     "The content below is all fictitious and is just a sample from 'https://github.com/uyu423/resume-nextjs'.",
-  //   icon: faBell,
-  // },
+  notice: {
+    title:
+      "The content below is all fictitious and is just a sample from 'https://github.com/uyu423/resume-nextjs'.",
+    icon: faBell,
+  },
 };
 
 export default profile;
